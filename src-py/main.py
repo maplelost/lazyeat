93313d0d48e2e33b9392558598c5d2b17ad04ce1
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-03-31 18:52:52 +0800

    Revert "fix opencv buffer dimension cause read img slow down"
    This reverts commit 0b08b7a46612d62cb80dd6e1cdb9a24bdcbef4fd.

--- src-py/main.py
+++ src-py/main.py
@@ -70,14 +70,8 @@ def thread_detect():
     # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))  # 优化帧率
     thread_cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # 设置编码格式
 
-    # fix opencv buffer dimension cause read img slow down
-    from opencv_ring_buffer import OpencvRingBuffer
-    opencv_ring_buffer = OpencvRingBuffer(thread_cam, ring_size=5)
-    opencv_ring_buffer.startcap()
-
     while True:
-        # success, img = thread_cam.read()
-        success, img = opencv_ring_buffer.getnew()
+        success, img = thread_cam.read()
 
         if my_detector is None:
             show_toast(
@@ -111,7 +105,11 @@ def thread_detect():
                     pass
 
     # 结束取图，释放资源
-    opencv_ring_buffer.stopcap()
+    try:
+        thread_cam.release()
+        cv2.destroyAllWindows()
+    except:
+        pass
 
 
 @app.get("/toggle_work")
